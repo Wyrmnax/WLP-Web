3f697f636c270d00386fbb645e92325d405bdf3c
Paulo Henrique Ribeiro Silva <phrsilva@br.ibm.com>
2017-05-03 12:15:31 -0300

    [WWLP] - Layout e ajuste do deploy

---
+++ code/src/Web Workload Pricer/wwlp.MVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wwlp.MVC.ViewModels.UI;
+
+namespace wwlp.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private HomeUI UI { get; set; }
+
+        public HomeController()
+        {
+            UI = new HomeUI();
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Dashboard";
+            ViewData["Message"] = "Esta é a view inicial";
+            return View(UI);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Title"] = "Sobre";
+            ViewData["Message"] = "Ésta é a view de about";
+            return View(UI);
+        }
+    }
+}
